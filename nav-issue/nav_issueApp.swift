1001314f64b680f2cae381dc855fcede13a2a2e6
Yifan Jin <yijin@expedia.com>
2023-11-22 09:08:16 +1000

    Initial Commit

---
+++ nav-issue/nav_issueApp.swift
@@ -0,0 +1,17 @@
+//
+//  nav_issueApp.swift
+//  nav-issue
+//
+//  Created by Yifan Jin on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct nav_issueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
